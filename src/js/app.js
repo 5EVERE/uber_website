fc3822a308966370e7861689511b3d20c602f742
5EVERE <severgvozd2005@gmail.com>
2023-02-06 00:08:24 +0200

    commit media changes

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,19 @@ document
     e.preventDefault();
     document.querySelector(".header").scrollIntoView({ behavior: "smooth" });
   });
+
+const menu = document.querySelector(".menu"),
+  menuItem = document.querySelectorAll(".menu_item"),
+  hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger_active");
+  menu.classList.toggle("menu_active");
+});
+
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("menu_active");
+  });
+});
